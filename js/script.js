69ea69df014bbc68645fbf6d406b8dc0821a3a60
Your Name <you@example.com>
2017-01-03 17:27:36 +0200

    add a background of navbar when page is scrolled

--- js/script.js
+++ js/script.js
@@ -2,26 +2,38 @@ window.onload = function() {
     var home = document.getElementById('home');
     home.style.minHeight = window.innerHeight + 'px';
     
-    
     var navbar = document.getElementById('navbar');
+    var homeText = document.getElementById('homeText');
+    var ghostButton1 = document.getElementById('ghostButton1');
+    var ghostButton2 = document.getElementById('ghostButton2');
+    
+    setTimeout(showNavbar, 1000);
+    setTimeout(showHomeText, 2000);
+    
+    
     function showNavbar(){
         navbar.style.visibility = 'visible';
         navbar.style.top = '0px';
     }    
-    setTimeout(showNavbar, 1000);
     
-    
-    var homeText = document.getElementById('homeText');
     function showHomeText() {
         homeText.style.opacity = '1';
     }
-    setTimeout(showHomeText, 2000);
+        
+window.onscroll = function() {myFunction()};
+    function myFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            navbar.style.backgroundColor = "black";
+            navbar.style.padding = "0 10%";
+        } else {
+            navbar.style.backgroundColor = "inherit";
+            navbar.style.padding = "20px 10%";
+        }
+    }  
     
-    var ghostButton1 = document.getElementById('ghostButton1');
-    var ghostButton2 = document.getElementById('ghostButton2');
-    
-    ghostButton1.onclick = function(){
-        window.scrollTo(0, 1000)
-    }
-    
+
+
+
+
 }
+
